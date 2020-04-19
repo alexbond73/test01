7c6983944f0db655e3d25df65e664e11e7fdfa5c
Alex Bondarenko <alexbond73@gmail.com>
2020-04-20 02:38:47 +0300

    Add memory allocation tests: with realloc an calloc

---
+++ test.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#define TRUE 1
+
+char* arr;
+int ctr = 1;
+int ctr_divisor = pow(2, 25);
+int i;
+
+// arr = (char*)malloc(1 * sizeof(char));
+
+int main() {
+  while(TRUE) {
+  ctr *= 2;
+  if((ctr % ctr_divisor) == 0) printf("%u : %u\n", ctr*sizeof(char), ctr);
+  arr = realloc(arr, ctr * sizeof(char));
+  if(arr == NULL) {
+  	printf("Can't allocate %u of memory", ctr*sizeof(char));
+	exit(0);
+  }
+  for(i = 0; i < ctr; i++) {
+     arr[i] = 'i';
+    }
+  }
+free(arr);
+}
